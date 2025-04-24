f33e833321bf7cd64e5742f370b1bf2cd42b3c0a
ur-fault <minefuf.fufik@gmail.com>
2025-04-24 22:37:43 +0200

    Check for chat ID in `get_message`

--- Telegram/server.py
+++ Telegram/server.py
@@ -4,6 +4,7 @@ import time
 
 latest_message = None
 
+
 class TelegramServer:
     def __init__(self):
         self.token = os.environ.get("telegram_api_key")
@@ -12,29 +13,29 @@ class TelegramServer:
         self.chat_id = os.environ.get("authorized_telegram_chat_id")
         self.debug = os.environ.get("debug") == "True"
         self.url = f"https://api.telegram.org/bot{self.token}"
-        self.last_message_time_stamp = time.time() # used to ony check for new messages received after the lest message was sent
+        self.last_message_time_stamp = (
+            time.time()
+        )  # used to ony check for new messages received after the lest message was sent
         self.last_msg_id = 0
         self.last_message_id = ""
 
     def send_message(self, message: str, required=True):
-        if (required or self.debug):
+        if required or self.debug:
             url = f"{self.url}/sendMessage"
-            body = {
-                "chat_id": self.chat_id,
-                "text": message
-            }
+            body = {"chat_id": self.chat_id, "text": message}
             response = requests.post(url, json=body)
             # print(response.text)
             self.last_message_time_stamp = time.time()
-        
+
     def get_message(self, fast=True):
         url = f"{self.url}/getUpdates"
-        body = {
-            "offset": self.last_msg_id
-        }
+        body = {"offset": self.last_msg_id}
         response = requests.post(url, json=body)
         last_result = response.json()["result"][-1]["message"]
-        if ((float(self.last_message_time_stamp) < float(last_result["date"])) and (self.last_message_id != last_result["message_id"])):
+        if (
+            (float(self.last_message_time_stamp) < float(last_result["date"]))
+            and (self.last_message_id != last_result["message_id"])
+        ) and str(self.chat_id) == str(last_result["chat"]["id"]):
             print("new message received")
             self.last_message_time_stamp = float(last_result["date"]) + float(1)
             self.last_message_id = last_result["message_id"]
@@ -43,8 +44,8 @@ class TelegramServer:
         else:
             time.sleep(0.5)
             if fast:
-                return self.get_message() # recursive call to recheck for new messages
-    
+                return self.get_message()  # recursive call to recheck for new messages
+
 
 # ts = TelegramServer()
 # ts.send_message("Server started")
